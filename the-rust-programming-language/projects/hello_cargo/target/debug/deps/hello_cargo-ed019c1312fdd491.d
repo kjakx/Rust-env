c92db168d717dac8e9483bf9162fe4d0be0b726d
kjakx <fxhfr163@ybb.ne.jp>
2020-05-05 14:57:15 +0000

    getting started

---
+++ the-rust-programming-language/projects/hello_cargo/target/debug/deps/hello_cargo-ed019c1312fdd491.d
@@ -0,0 +1,5 @@
+/workspace/Rust-env/the-rust-programming-language/projects/hello_cargo/target/debug/deps/hello_cargo-ed019c1312fdd491: src/main.rs
+
+/workspace/Rust-env/the-rust-programming-language/projects/hello_cargo/target/debug/deps/hello_cargo-ed019c1312fdd491.d: src/main.rs
+
+src/main.rs:
---
+++ the-rust-programming-language/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/workspace/Rust-env/the-rust-programming-language/projects/hello_cargo/target/debug/hello_cargo: /workspace/Rust-env/the-rust-programming-language/projects/hello_cargo/src/main.rs
