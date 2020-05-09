c1454ec4523b3bb1ae4cccc02a58f5aa96952eb1
kjakx <fxhfr163@ybb.ne.jp>
2020-05-09 19:18:19 +0000

    rectangles

---
+++ the-rust-programming-language/projects/rectangles/target/debug/deps/rectangles-e8d39ba18bf4261a.d
@@ -0,0 +1,5 @@
+/workspace/Rust-env/the-rust-programming-language/projects/rectangles/target/debug/deps/rectangles-e8d39ba18bf4261a: src/main.rs
+
+/workspace/Rust-env/the-rust-programming-language/projects/rectangles/target/debug/deps/rectangles-e8d39ba18bf4261a.d: src/main.rs
+
+src/main.rs:
---
+++ the-rust-programming-language/projects/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/workspace/Rust-env/the-rust-programming-language/projects/rectangles/target/debug/rectangles: /workspace/Rust-env/the-rust-programming-language/projects/rectangles/src/main.rs
